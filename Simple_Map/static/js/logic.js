fe06b09810e01e6fb80996baa23a1d65930fc450
Brian Morrison <mbrian@gmail.com>
2021-01-18 17:07:32 -0600

    test

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,13 +2,8 @@
 console.log("working");
 
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -19,3 +14,41 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+//L.geoJson(sanFranAirport, {
+//  // We turn each feature into a marker on the map.
+//  pointToLayer: function(feature, latlng) {
+//    console.log(feature);
+//    return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + "</h2>");
+//  }
+
+//}).addTo(map);
+
+//Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer)
+    layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+  }
+}).addTo(map);
